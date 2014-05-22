1996d22e6a8b8ace37c63a1ea2ad89313b8010a5
i.pavlov <i.pavlov@2gis.ru>
2014-05-22 17:31:32 +0700

    one more fix

--- build_packer.sh
+++ build_packer.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-git clone https://github.com/mitchellh/packer.git $GOPATH/src/github.com/mitchellh/packer
-cd $GOPATH/src/github.com/mitchellh/packer
+git clone https://github.com/mitchellh/packer.git
+cd packer
 make
