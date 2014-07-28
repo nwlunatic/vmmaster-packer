2bb37fa0a4e0d024150c459a4f05c6b1ef51ec15
i.pavlov <i.pavlov@2gis.ru>
2014-07-28 17:59:06 +0700

    fix install pip on win

--- windows/scripts/software.bat
+++ windows/scripts/software.bat
@@ -1,8 +1,19 @@
 call a:\resetvars.bat
-
-cinst git
-cinst GoogleChrome
-cinst Firefox
-cinst javaruntime
+choco install python -Version 2.7.6
+call a:\resetvars.bat
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "((new-object net.webclient).DownloadString('https://bootstrap.pypa.io/get-pip.py'))" > get-pip.py
+python get-pip.py
+setx PATH "%PATH%;C:\Python27\Scripts;"
+call a:\resetvars.bat
 cinst PyWin32
-cinst pip
+
+call a:\resetvars.bat
+cinst git
+
+call a:\resetvars.bat
+cinst javaruntime
+
+call a:\resetvars.bat
+cinst GoogleChrome
+call a:\resetvars.bat
+cinst Firefox
