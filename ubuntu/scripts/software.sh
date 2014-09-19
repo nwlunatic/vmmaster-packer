3bd65797a046f00b7816e098b76b0a65e4ebd93f
i.pavlov <i.pavlov@2gis.ru>
2014-09-19 17:46:26 +0700

    add memory and cpu variables

--- ubuntu/scripts/software.sh
+++ ubuntu/scripts/software.sh
@@ -25,4 +25,4 @@ chown -R vagrant:vagrant ~/selenium-launchers
 # install google chrome
 cd /tmp
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb || sudo apt-get -f -y install
